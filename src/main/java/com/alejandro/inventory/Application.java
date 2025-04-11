3ce06dd7b9ab7e1ba001d97412ed5ba4408c9019
Alejandro <alejandromontano@useit.co>
2025-04-11 10:07:52 -0500

    adding swagger

--- src/main/java/com/alejandro/inventory/Application.java
+++ src/main/java/com/alejandro/inventory/Application.java
@@ -2,8 +2,27 @@ package com.alejandro.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Inventory API",
+		version = "1.0",
+		description = "API para la gestión de inventario y ventas",
+		contact = @Contact(
+			name = "Alejandro",
+			email = "contacto@ejemplo.com"
+		),
+		license = @License(
+			name = "Apache 2.0",
+			url = "https://www.apache.org/licenses/LICENSE-2.0"
+		)
+	)
+)
 public class Application {
 
 	public static void main(String[] args) {
